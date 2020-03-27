795c9e6bf135c06602bf3a1d78021ec43a5f711b
Krunal Patel <Pkrunal616@gmail.com>
2020-03-27 13:23:32 -0400

    name and ring_buffer done

--- names/names.py
+++ names/names.py
@@ -2,25 +2,31 @@ import time
 
 start_time = time.time()
 
-f = open('names_1.txt', 'r')
+f = open("names_1.txt", "r")
 names_1 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-f = open('names_2.txt', 'r')
+f = open("names_2.txt", "r")
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+
+# check if the both list has same amount name
+# loop throw name_1 and check if that name is also in name_2
+# if it is put in the list, if not loop
+if len(names_1) == len(names_2):
+    print(True)
+
+for names_1s in names_1:
+    if names_1s in names_2:
+        duplicates.append(names_1s)
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
